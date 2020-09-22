3b27ab56dfa511f86cf614b5c57392fd84630075
Lance3092 <57061398+Lance3092@users.noreply.github.com>
2020-09-22 19:35:22 +1000

    updated project files

---
+++ PHP/PHP.DataBase.TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHP.DataBase.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
